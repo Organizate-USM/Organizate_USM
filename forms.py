1027590c640def0a2267574cc2c6b45397d75bb2
FelipeGuicharrousse <felipe.guicharrousse@usm.cl>
2020-11-14 19:14:24 -0300

    agregar avence de login e inicio de session

--- forms.py
+++ forms.py
@@ -1,8 +1,28 @@
 from wtforms import Form
 from wtforms import StringField, TextField
 from wtforms.fields.html5 import EmailField
+from wtforms import HiddenField
+from wtforms import TextAreaField
+from wtforms import PasswordField
+
+
+from wtforms import validators
+
+def length_honeypot(form, field):
+    if len(field.data) > 0:
+        raise validators.ValidationError('El campo debe estar vacio')
 
 class CommentForm(Form):
-    username = StringField('username')
-    email = EmailField('Correo electronico')
-    comment = TextField('Comentario')
+	comment = TextAreaField('Comentario')
+	honeypot = HiddenField('', [length_honeypot])
+
+class LoginForm(Form):
+	username = StringField('Username',
+				[
+				validators.Required(message = 'El username es requerido!.'),
+				validators.length(min=4, max=25, message='Ingrese un username valido!.'),
+				])
+	password = PasswordField('Password',
+                            [
+                            validators.Required(message='El password es requerido')
+                            ])
--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import make_response
+from flask import session
+from flask import url_for
+from flask import redirect
+from flask import flash
 
 import forms
 
 app = Flask(__name__)
+app.secret_key = 'Llave_secreta'
 
 @app.route('/')
 def index():
-    comment_form = forms.CommentForm()
-    title = "Organizate USM"
-    return render_template('index.html', title=title, form=comment_form)
+	if 'username' in session:
+		username = session['username']
+	title = 'Index'
+	return render_template('index.html', title = title)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    login_form = forms.LoginForm(request.form)
+    if request.method == 'POST' and login_form.validate():
+        username = login_form.username.data
+        success_message = 'Bienvenido {}'.format(username)
+        flash(success_message)
+        session['username'] = login_form.username.data
+    return render_template('login.html', form = login_form)
+
+@app.route('/cookie')
+def cookie():
+    response = make_response( render_template('cookie.html'))
+    response.set_cookie('custome_cookie', 'Felipe')
+    return response
+
+@app.route('/logout')
+def logout():
+    if "username" in session:
+        session.pop('username')
+    return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
