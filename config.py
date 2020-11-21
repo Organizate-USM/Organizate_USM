818ddacceedbbcb5a65e05cb4ddc94879e2d138d
Jonathan Olivares Salinas <jonathan.olivaress@usm.cl>
2020-11-21 19:47:06 -0300

    vuelta sqlite y el otro comentado
    A

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ class Config(object):
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/flaskmysql'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/flaskmysql'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///C:\db_organizate\organizate.db'
