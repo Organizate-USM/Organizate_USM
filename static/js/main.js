689a25f6aa29f979e86633255bec31b6e85de9e5
Gabo <gabriel.vergaraa@usm.cl>
2020-12-07 18:15:29 -0300

    pomodoro casi listo

--- static/js/main.js
+++ static/js/main.js
@@ -16,8 +16,8 @@ function start_timer() {
   click_sound.play();
 
   // Change the minutes and seconds to starting time
-  session_minutes = 24;
-  session_seconds = 59;
+  session_minutes = 0;
+  session_seconds = 10;
 
   // Add the seconds and minutes to the page
   document.getElementById("minutes").innerHTML = session_minutes;
@@ -49,7 +49,7 @@ function start_timer() {
 
         // Add the message to the html
         document.getElementById("done").innerHTML =
-          "Session Completed!! Take a Break";
+          "Sesión completada!! Toma un descanso";
 
         // Make the html message div visible
         document.getElementById("done").classList.add("show_message");
